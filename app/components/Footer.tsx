48da74217c20f77e4aa819e2d2979feae4180a93
abdrizik <abdelrahman.rizik@gmail.com>
2025-06-24 03:18:05 +0300

    feat: enhance footer with a link to the author's profile

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export function Footer() {
   return (
     <footer className='w-full mt-20 py-4 text-gray-500'>
       <div className='flex flex-col md:flex-row justify-between items-center'>
-        <div className='mb-4 md:mb-0'>Made with ❤️ by @abdrikik</div>
+        <div className='mb-4 md:mb-0'>
+          Made with ❤️ by{' '}
+          <Link
+            to='https://bsky.app/profile/abdrizik.bsky.social'
+            target='_blank'
+          >
+            @abdrikik
+          </Link>
+        </div>
         <div>© {new Date().getFullYear()} All rights reserved.</div>
       </div>
     </footer>
